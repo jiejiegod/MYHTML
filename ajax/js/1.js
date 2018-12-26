99b802a6fe8e44d3b9c1033a89f0bcccb93b4703
jiejiegod <44337992+jiejiegod@users.noreply.github.com>
2018-12-26 19:13:05 +0800

    Update 1.js

--- ajax/js/1.js
+++ ajax/js/1.js
@@ -1,8 +1,8 @@
 $(function(){
-	$("#header").load("txt/txt1.txt #img1")
-	$("#menu").load("txt/txt1.txt #ul1")
-	$("#nav").load("txt/txt1.txt #left")
-	$("#footer").load("txt/txt1.txt #foot")
-	$("#content").load("txt/txt1.txt #div1")
+	$("#header").load("txt1.txt #img1")
+	$("#menu").load("txt1.txt #ul1")
+	$("#nav").load("txt1.txt #left")
+	$("#footer").load("txt1.txt #foot")
+	$("#content").load("txt1.txt #div1")
 
 })
