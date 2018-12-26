e0a2910ce56e904ce346bd7c3e095dd08517922f
jiejiegod <44337992+jiejiegod@users.noreply.github.com>
2018-12-26 19:06:58 +0800

    Add files via upload

---
+++ ajax/js/1.js
@@ -0,0 +1,8 @@
+$(function(){
+	$("#header").load("txt/txt1.txt #img1")
+	$("#menu").load("txt/txt1.txt #ul1")
+	$("#nav").load("txt/txt1.txt #left")
+	$("#footer").load("txt/txt1.txt #foot")
+	$("#content").load("txt/txt1.txt #div1")
+
+})
