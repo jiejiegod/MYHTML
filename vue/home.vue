7683dbb69eb74aeba586d0f2ec3e79d55a6e08bf
jiejiegod <44337992+jiejiegod@users.noreply.github.com>
2019-11-18 10:06:56 +0800

    Create home.vue

---
+++ vue/home.vue
@@ -0,0 +1,26 @@
+<template>
+     <div>
+         <home-header></home-header>
+         <home-swiper></home-swiper>
+         <home-icons></home-icons>
+     </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+//添加 HomeIcons组件
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
